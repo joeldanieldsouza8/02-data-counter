570bc5ee97b51bf040ef5013f3e6379ef101fb2a
Joel Dsouza <joeldsouza8@gmail.com>
2023-10-21 13:01:51 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Counter />
   );
 }
 
-export default App;
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  // 'count' and 'date' will always be updated at the same time.
+  // 'date depends on 'count', so it should be updated in the same batch.
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  function handleDecrease() {
+    if (count > 0) {
+      setCount((prevCount) => prevCount - step);
+    }
+  }
+
+  function handleIncrease() {
+    setCount((prevCount) => prevCount + step);
+  }
+
+  function handleStepDecrease() {
+    if (step > 1) {
+      setStep((prevStep) => prevStep - 1);
+    }
+  }
+
+  function handleStepIncrease() {
+    setStep((prevStep) => prevStep + 1);
+  }
+
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
+
+  function displayDate() {
+    if (count === 0) {
+      return "Today is ";
+    } 
+
+    else if (count >= 1) {
+      return `${count} days from today is `;
+    }
+    
+    else {
+      return `${count} days ago was `;
+    }
+  }
+
+  
+
+  return (
+    <div>
+      <div>
+        <button onClick={handleStepDecrease}>-</button>
+        <span>Step: {step}</span>
+        <button onClick={handleStepIncrease}>+</button>
+      </div>
+
+      <div>
+        <button onClick={handleDecrease}>-</button>
+        <span>Count: {count}</span>
+        <button onClick={handleIncrease}>+</button>
+      </div>
+
+      <div>
+        <span>{displayDate()}</span>
+        <span>{date.toLocaleDateString()}</span>
+      </div>
+
+      <div className="reset-button">
+        <button onClick={handleReset}>Reset</button>
+      </div>
+    </div>
+  );
+}
